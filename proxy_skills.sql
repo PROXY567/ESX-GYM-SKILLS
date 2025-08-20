777f44e4ec8dc09225f0511bd2f75a8a2c4551ab
PROXY567 <harleythorpe168@gmail.com>
2025-08-21 00:36:21 +0100

    Delete proxy_skills.sql

--- proxy_skills.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `proxy_skills` (
-  `identifier` VARCHAR(60) NOT NULL,
-  `stamina` INT NOT NULL DEFAULT 0,
-  `strength` INT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
